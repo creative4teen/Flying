84257465474266cafaec4e00efe19eb6f6a90554
creative4teen <creative4teen@gmail.com>
2018-03-01 10:14:31 -0500

    m

--- Flying/Assets/Script/Env.cs
+++ Flying/Assets/Script/Env.cs
@@ -6,21 +6,29 @@ public class Env : MonoBehaviour {
 
     public GameObject[] planes;
 
+    public GameObject[] go100m;
+
     public GameObject figures;
 
     private int currentPlaneIndex;
     private GameObject current3DObject;
 
-    private GameObject mainCamera;
-    private bool zoom = false;
+    private GameObject goVR;
+    private Camera mainCamera;
+    private int zoom = 1;
+    private float[] layerCullDistances;
 
     private AudioSource music;
     private bool musicOn = true;
 
 
     private void Awake() {
-        mainCamera = this.transform.Find("VR").gameObject;
-        mainCamera.SetActive(true);
+        goVR = this.transform.Find("VR").gameObject;
+        goVR.SetActive(true); 
+            
+        mainCamera = goVR.transform.Find("GvrHeadset/Camera").GetComponent<Camera>();
+
+        mainCamera.layerCullDistances[8] = 100;
         music = this.transform.GetComponent<AudioSource>();
     }
 
@@ -33,18 +41,13 @@ public class Env : MonoBehaviour {
 
     // Update is called once per frame
     void FixedUpdate () {
-        Vector3 pos = Vector3.zero;
+        {
+            Vector3 pos = Vector3.zero;
 
-        if (zoom) {
-            pos = new Vector3(current3DObject.transform.position.x + 15, current3DObject.transform.position.y + 5, current3DObject.transform.position.z - 10);
+            pos = new Vector3(current3DObject.transform.position.x + (1 + zoom) * 2, current3DObject.transform.position.y + (1 + zoom) * 5, current3DObject.transform.position.z - (1 + zoom) * 12);
+
+            goVR.transform.position = pos;
         }
-        else {
-            pos = new Vector3(current3DObject.transform.position.x + 45, current3DObject.transform.position.y + 5, current3DObject.transform.position.z - 35);
-        }
-
-        Transform trans = mainCamera.transform;
-        trans.position = pos;
-
     }
 
     public void switchPlane() {
@@ -59,7 +62,7 @@ public class Env : MonoBehaviour {
 
 
     public void zoomChange() {
-        zoom = !zoom;
+        zoom = (zoom+1) % 6;
     }
 
     public void musicChange() {
--- Flying/Assets/Script/PlaneAction.cs
+++ Flying/Assets/Script/PlaneAction.cs
@@ -3,12 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlaneAction : MonoBehaviour {
+    //
+    public float centerX = 0f;
+    public float centerY = 0f;
+    public float centerZ = 0f;
 
+    public static Vector3 ZERO = new Vector3(0, 0, 0);
+    //public float RotationSpeed = 0.1f;
+    public float OrbitDegrees = 0.001f;
+    //
+    public float tiltX = 0f;
+    public float tiltY = 1f;
+    public float tilt = 0f;
+    //
     private bool action = true;
     private float balancedTime;
     private float rotationleft;
     private float rotationSpeed;
 
+    private Collider collider;
+
+    //
+    private void Awake() {
+        collider = this.GetComponent<Collider>();
+        ZERO = new Vector3(centerX, centerY, centerZ);
+    }
     // Use this for initialization
     void Start () {
         action = true;
@@ -20,13 +39,21 @@ public class PlaneAction : MonoBehaviour {
 
 
     // Update is called once per frame
-    void FixedUpdate () {
+    void FixedUpdate() {
+        CircleAround();
+        if (collider != null) {
+            CollisionAction();
+        }
+    }
+
+    //
+    private void CollisionAction() { 
         if (action) { 
             if (transform.localEulerAngles.z < 80 || transform.localEulerAngles.z > 90) {
                 transform.Rotate(Vector3.forward * Time.deltaTime * 80);
             }
             balancedTime = 0;
-            rotationleft = 360 * Random.Range(1, 5);
+            rotationleft = 360 * Random.Range(-3, 4);
             rotationSpeed = 240 * Random.Range(0.33333f, 1);
         }
         else {
@@ -50,6 +77,12 @@ public class PlaneAction : MonoBehaviour {
         }
     }
 
+    //
+    private void CircleAround() {
+        //transform.Rotate(Vector3.up, RotationSpeed * Time.deltaTime);
+        Vector3 line = new Vector3(tiltX, tiltY, tilt);  // up (0, 1, -0.35f).
+        transform.RotateAround(ZERO, line, OrbitDegrees * Time.deltaTime);
+    }
 
     void OnTriggerEnter(Collider collision) {
         action = true;
